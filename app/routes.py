f58ef4a0f58fa34534729f1a39068d4ff6624e8f
AlejandroRodrigueP <a.rodriguez.p.1995@gmail.com>
2020-07-01 21:31:48 -0500

    pdf name change is correct

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,18 @@
 import os
 import json
-from flask import render_template, request, jsonify
+from flask import render_template, request, jsonify, send_file, send_from_directory
 from app import app
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import requests
+
 from pdf2image import convert_from_bytes
+from pdf2image.exceptions import (
+    PDFInfoNotInstalledError,
+    PDFPageCountError,
+    PDFSyntaxError
+)
+
+
 
 @app.route('/')
 def start():
@@ -16,61 +24,41 @@ def upload():
     parent_file= (request.form.get('parent_pdf_id'))
     workflow_id= (request.form.get('workflow_id'))
     pdf_data = None 
-    xtracta_ids = list()
+    doc_images = list()
     #return "test", 200
     if 'pdf' in request.files:
         incoming_pdf = request.files['pdf']
         pdf_data = PdfFileReader(incoming_pdf, 'rb')
         output = PdfFileWriter()
         output.addPage(pdf_data.getPage(0))
-        with open("document-page0.pdf", "wb") as outputStream:
-            output.write(outputStream)
-            print('Created: {}'.format("document-page0.pdf"))
-            outputStream.close()
-
-        with open("document-page0.pdf", "rb") as file_to_send:
-            print(file_to_send)
-            files = {'userfile': file_to_send}
-            upload_url ='https://api-app.xtracta.com/v1/documents/upload'
-            auth_upload = {
-            'api_key':'b65d6427252e69e4aa29728f6ebfbf43ccf2f266',
-            'workflow_id': workflow_id
-            }
-
-            r=requests.post(url=upload_url, files=files,data=auth_upload)
-            xtracta_id = r.content[115:124]
-            xtracta_ids.append(xtracta_id)
-            file_to_send.close()
-
-        with open("document-page0.pdf", "rb") as pdf_to_upload:
-            knack_headers= {
-                'x-knack-rest-api-key': '1c6ec550-7526-11ea-9edc-dd38c98162a1',
-                'X-Knack-Application-ID': '5e8632e077f0c200158408ff'
-            }
-            record_url= 'https://api.knack.com/v1/objects/object_4/records/'+ str(parent_file)
-            record_data= {
-                'field_41': xtracta_id,
-            }
-            try:
-                record_r= requests.put(url=record_url, headers=knack_headers, data=record_data)
-            except requests.exceptions.RequestException as e:
-                print(e)
-                record_r= requests.put(url=record_url, headers=knack_headers, data=record_data)
-            print(record_r.content)
-            pdf_to_upload.close() 
+        for i in range(0,pdf_data.numPages,1):
+            output.addPage(pdf_data.getPage(i))
+            page = i + 1
             
-        os.remove("document-page0.pdf")
+            with open("document-page%s.pdf" % i, "wb") as outputStream:
+                output.write(outputStream)
+                print('Created: {}'.format("document-page%s.pdf" % i))
+                outputStream.close()
+
+           # with open("document-page%s.pdf" % i,"rb") as doc_page:
+            #    images = convert_from_bytes(doc_page.read(), fmt='png')
+             #   doc_images.append(images)
+            
+
+            
+            
+            os.remove("document-page%s.pdf" % i)
     else:
         return "please upload a file to process" , 403
+    
+    with open("output_INV.pdf", "wb") as outputStream:
+        path ='/home/smokeythebear/Documents/Python/cimarex/output_INV.pdf'
+        output.write(outputStream)
+        #print('Created: {}'.format("document-page%s.pdf" % i))
+        outputStream.close()
 
-    return "success"
+        return send_file(path, as_attachment=True, mimetype='application/pdf')
 
 
 
-@app.route('/change', methods=['POST'])
-def change():
-    if 'pdf' in request.files:
-        incoming_pdf = request.files['pdf']
-        os.rename(incoming_pdf, 'output_INV.pdf')
-        return send_file('output_INV.pdf',mimetype='application/pdf')
-        # return
+       
