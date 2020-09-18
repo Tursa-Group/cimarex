2b5e0dc8d23e87ee3b3200a07a5972944cae4c7d
Jazz <jazzcelaya@gmail.com>
2020-09-17 23:58:46 -0500

    adds element endpoint

--- app/routes.py
+++ app/routes.py
@@ -103,3 +103,51 @@ def addrates():
     else:
         print('data not reaching processing')
         return 'please include the records data'
+
+
+@app.route('/add_elm_rates', methods = ['POST'])
+def add_elm_rates():
+
+    incoming_data = request.json
+    service_ticket = None
+    rate_group = None
+    labour_trades = []
+    equipment_trades = []
+
+    if 'id' in incoming_data[0]:
+        service_ticket = incoming_data[0]['id']
+        rate_group = incoming_data[0]['field_242_raw'][0]['id']
+        labour_rates_url = 'https://api.knack.com/v1/objects/object_15/records?filters=%5B%7B%22field%22%3A%22field_225%22%2C%22operator%22%3A%22is%22%2C%22value%22%3A%22{}%22%7D%5D'.format(rate_group)
+        equipment_rates_url = 'https://api.knack.com/v1/objects/object_33/records?filters=%5B%7B%22field%22%3A%22field_225%22%2C%22operator%22%3A%22is%22%2C%22value%22%3A%22{}%22%7D%5D'.format(rate_group)
+        service_ticket_url = 'https://api.knack.com/v1/objects/object_2/records/{}'.format(service_ticket)
+        auth_upload = {
+            'X-Knack-Application-Id':'5ed9190db05e020015611d46',
+            'X-Knack-REST-API-KEY':'6d801ed0-a67c-11ea-96bb-196128bd147e'
+            }
+        r=requests.get(url=labour_rates_url,headers=auth_upload)
+        response = json.loads(r.text)
+        labour_records = response['records']
+        print(labour_records)
+
+        for record in labour_records:
+            labour_trades.append(record['id'])
+
+        equipment_records = requests.get(url=equipment_rates_url,headers=auth_upload)
+        e_response = json.loads(equipment_records.text)
+        equipment_records = e_response['records']
+
+        for record in equipment_records:
+            equipment_trades.append(record['id'])
+
+        update_data = {
+            'field_244': labour_trades,
+            'field_252': equipment_trades
+        }
+        update_record = requests.put(url=service_ticket_url,headers=auth_upload, json=update_data)
+        update_response = json.loads(update_record.text)
+        print(update_response)
+        return str(update_response)
+
+    else:
+        print('data not reaching processing')
+        return 'please include the records data'
