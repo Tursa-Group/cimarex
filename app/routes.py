bd0ea07ba2174b33753cd3b8e94e8501a0e30db8
Jazz Celaya <40035747+jazzcelaya@users.noreply.github.com>
2021-04-30 13:30:51 -0500

    Corrects equipment rate sheet connection

--- app/routes.py
+++ app/routes.py
@@ -122,7 +122,7 @@ def add_elm_rates():
         rate_group = incoming_data[0]['field_242_raw'][0]['id']
         labour_rates_url = 'https://api.knack.com/v1/objects/object_15/records?filters=%5B%7B%22field%22%3A%22field_225%22%2C%22operator%22%3A%22is%22%2C%22value%22%3A%22{}%22%7D%5D'.format(
             rate_group)
-        equipment_rates_url = 'https://api.knack.com/v1/objects/object_33/records?filters=%5B%7B%22field%22%3A%22field_225%22%2C%22operator%22%3A%22is%22%2C%22value%22%3A%22{}%22%7D%5D'.format(
+        equipment_rates_url = 'https://api.knack.com/v1/objects/object_33/records?filters=%5B%7B%22field%22%3A%22field_251%22%2C%22operator%22%3A%22is%22%2C%22value%22%3A%22{}%22%7D%5D'.format(
             rate_group)
         service_ticket_url = 'https://api.knack.com/v1/objects/object_2/records/{}'.format(
             service_ticket)
